77016ef6f69d22f16df39a4a5d0c7182da026293
Julio Gleison da Silva <julio.gleison68@gmail.com>
2026-03-27 21:10:35 +0000

    up das modificações que não estavam sendo salvas por erro de permissão de usuário

--- app/meu_projeto/settings.py
+++ app/meu_projeto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m71ablggk%y&^6mgt)ct67n5pa02_7#jjg7uya3&mwfy$r=7+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nucleo',
 ]
 
 MIDDLEWARE = [
--- app/nucleo/views.py
+++ app/nucleo/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Alô professor, sou Julio Gleison da Silva da turma SO 2025.2</h1>")
